248091f99db246fdf60ff8f9532a882589ab9ded
Marek <zylamarek@gmail.com>
2019-07-15 14:01:48 +0200

    bounding box training

---
+++ predictor/cat_data_generator.py
@@ -0,0 +1,66 @@
+import numpy as np
+import keras
+from keras.applications import mobilenet_v2
+import os
+from PIL import Image
+
+
+class CatDataGenerator(keras.utils.Sequence):
+    def __init__(self, paths, batch_size=64, img_size=224, shuffle=True):
+        self.paths = paths
+        self.batch_size = batch_size
+        self.img_size = img_size
+        self.shuffle = shuffle
+        self.output_dim = 4
+        self.img_shape = (self.img_size, self.img_size, 3)
+
+        self.files = []
+        for path in self.paths:
+            self.files.extend([os.path.join(path, f) for f in os.listdir(path) if f.endswith('.jpg')])
+
+        self.on_epoch_end()
+
+    def __getitem__(self, index):
+        indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
+        X = np.zeros((len(indexes),) + self.img_shape)
+        y = np.zeros((len(indexes), self.output_dim))
+
+        for i, idx in enumerate(indexes):
+            img_file = self.files[idx]
+            img = Image.open(img_file)
+            with open(img_file + '.cat', 'r') as cat:
+                landmarks = np.array([float(i) for i in cat.readline().split()[1:]]).reshape((-1, 2))
+
+            img, landmarks = self._resize_img(img, landmarks)
+            landmarks = np.round(landmarks).astype('int')
+            y[i] = np.concatenate([np.min(landmarks, axis=0), np.max(landmarks, axis=0)])
+            X[i] = np.asarray(img)
+
+        X = mobilenet_v2.preprocess_input(X)
+
+        return X, y
+
+    def _resize_img(self, img, landmarks):
+        old_size = img.size
+        if old_size != (self.img_size, self.img_size):
+            ratio = float(self.img_size) / max(old_size)
+            new_size = tuple([int(x * ratio) for x in old_size])
+            old_img = img.resize(new_size, Image.LANCZOS)
+
+            img = Image.new('RGB', (self.img_size, self.img_size))
+            x_diff = (self.img_size - new_size[0]) // 2
+            y_diff = (self.img_size - new_size[1]) // 2
+            img.paste(old_img, (x_diff, y_diff))
+
+            landmarks *= ratio
+            landmarks += np.array((x_diff, y_diff))
+
+        return img, landmarks
+
+    def __len__(self):
+        return int(np.ceil(len(self.files) / self.batch_size))
+
+    def on_epoch_end(self):
+        self.indexes = np.arange(len(self.files))
+        if self.shuffle:
+            np.random.shuffle(self.indexes)
---
+++ predictor/train_bb.py
@@ -0,0 +1,62 @@
+from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
+import datetime
+from keras.applications import mobilenet_v2
+from keras.layers import Dense
+from keras.models import Model, load_model
+import argparse
+import keras
+import os
+
+from cat_data_generator import CatDataGenerator
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--lr', default=0.001, type=float)
+    parser.add_argument('--units', default=128, type=int)
+    parser.add_argument('--epochs', default=100, type=int)
+    parser.add_argument('--batch_size', default=32, type=int)
+    args = parser.parse_args()
+
+    path = os.path.join('data', 'cat-dataset')
+
+    img_size = 224
+    exp_name = datetime.datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
+    exp_name += '_%.5f_%d_%d' % (args.lr, args.units, args.batch_size)
+    os.makedirs('models', exist_ok=True)
+    model_path = os.path.join('models', '%s.h5' % exp_name)
+
+    pretrained_net = mobilenet_v2.MobileNetV2(input_shape=(img_size, img_size, 3),
+                                              include_top=False, pooling='max', weights='imagenet')
+
+    path_train = [os.path.join(path, 'CAT_0%d' % i) for i in range(5)]
+    datagen_train = CatDataGenerator(paths=path_train, shuffle=True, batch_size=args.batch_size)
+    path_val = [os.path.join(path, 'CAT_0%d' % i) for i in (5,)]
+    datagen_val = CatDataGenerator(paths=path_val, shuffle=False, batch_size=args.batch_size)
+    path_test = [os.path.join(path, 'CAT_0%d' % i) for i in (6,)]
+    datagen_test = CatDataGenerator(paths=path_test, shuffle=False, batch_size=args.batch_size)
+
+    output_dim = 4
+    print('output_dim', output_dim)
+
+    outp = Dense(args.units, activation='relu')(pretrained_net.output)
+    outp = Dense(args.units, activation='relu')(outp)
+    outp = Dense(output_dim, activation='linear')(outp)
+    model = Model(inputs=pretrained_net.input, outputs=outp)
+
+    model.compile(optimizer=keras.optimizers.Adam(lr=args.lr), loss='mse')
+
+    model.summary()
+
+    train_history = model.fit_generator(generator=datagen_train, epochs=args.epochs, shuffle=True,
+                                        validation_data=datagen_val,
+                                        callbacks=[
+                                            TensorBoard(log_dir=os.path.join('logs', exp_name)),
+                                            ReduceLROnPlateau(factor=0.5, patience=5, verbose=1),
+                                            EarlyStopping(patience=13, verbose=1),
+                                            ModelCheckpoint(model_path, verbose=1, save_best_only=True)
+                                        ]
+                                        )
+
+    model = load_model(model_path)
+    test_eval = model.evaluate_generator(datagen_test)
+    print(test_eval)
