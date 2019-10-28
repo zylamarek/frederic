f961d4a3254e2e13d716fe3052e2e6303b2c9755
Marek <zylamarek@gmail.com>
2019-10-28 11:37:41 +0100

    added convenience hpsearch scripts

---
+++ hpsearch.bat
@@ -0,0 +1,17 @@
+@echo off
+
+cd tools
+
+python train.py --loss_fn iou --flip_horizontal --crop_scale_balanced --rotate_n 20 --ReduceLROnPlateau_factor 0.8 --ReduceLROnPlateau_patience 10
+python train.py --loss_fn iou --flip_horizontal --crop_scale_balanced --rotate_n 20 --ReduceLROnPlateau_factor 0.8 --ReduceLROnPlateau_patience 10
+python train.py --loss_fn iou --flip_horizontal --crop_scale_balanced --rotate_n 20 --ReduceLROnPlateau_factor 0.8 --ReduceLROnPlateau_patience 10
+python train.py --loss_fn iou --flip_horizontal --crop_scale_balanced --rotate_n 20 --ReduceLROnPlateau_factor 0.8 --ReduceLROnPlateau_patience 10
+python train.py --loss_fn iou --flip_horizontal --crop_scale_balanced --rotate_n 20 --ReduceLROnPlateau_factor 0.8 --ReduceLROnPlateau_patience 10
+
+python train.py --output_type landmarks --flip_horizontal --crop --rotate_n 20 --ReduceLROnPlateau_factor 0.8 --ReduceLROnPlateau_patience 10
+python train.py --output_type landmarks --flip_horizontal --crop --rotate_n 20 --ReduceLROnPlateau_factor 0.8 --ReduceLROnPlateau_patience 10
+python train.py --output_type landmarks --flip_horizontal --crop --rotate_n 20 --ReduceLROnPlateau_factor 0.8 --ReduceLROnPlateau_patience 10
+python train.py --output_type landmarks --flip_horizontal --crop --rotate_n 20 --ReduceLROnPlateau_factor 0.8 --ReduceLROnPlateau_patience 10
+python train.py --output_type landmarks --flip_horizontal --crop --rotate_n 20 --ReduceLROnPlateau_factor 0.8 --ReduceLROnPlateau_patience 10
+
+cd ..
